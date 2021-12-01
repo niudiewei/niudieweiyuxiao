b43951b161d9fd2ca0fa88b990dfebe4a7637184
YuXiao0111 <Yuxiao_0111@163.com>
2021-12-01 23:12:32 +0800

    整理代码

--- string.h
+++ string.h
@@ -3,11 +3,11 @@
 
 class String
 {
-public:                                 
-   String(const char* cstr=0);                     
-   String(const String& str);                    
-   String& operator=(const String& str);         
-   ~String();                                    
+public:
+   String(const char* cstr=0);
+   String(const String& str);
+   String& operator=(const String& str);
+   ~String();
    char* get_c_str() const { return m_data; }
 private:
    char* m_data;
@@ -22,7 +22,7 @@ String::String(const char* cstr)
       m_data = new char[strlen(cstr)+1];
       strcpy(m_data, cstr);
    }
-   else {   
+   else {
       m_data = new char[1];
       *m_data = '\0';
    }
